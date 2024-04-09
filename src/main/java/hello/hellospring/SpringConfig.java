a735f3334e75dc2bb32864febe97ead050b46ec3
Tech5818 <pqow5818@gmail.com>
2024-04-09 19:51:01 +0900

    :sparkles: 회원 repository jpa 구현

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,10 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcTmplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +13,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -26,6 +28,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcTmplateMemberRepository(dataSource);
+//        return new JdbcTmplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
